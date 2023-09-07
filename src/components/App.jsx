eb9331dd3461099991b3c0af50221cdd6335b11a
skrinskanastya <nastiolla@gmail.com>
2023-09-07 17:37:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics';
 import { Section } from './Section';
